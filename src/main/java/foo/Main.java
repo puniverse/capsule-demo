c63820c94b0aaedc72230033d166a9aaa07b6ed5
pron <ron.pressler@gmail.com>
2014-11-21 15:26:32 -0500

    -

--- src/main/java/foo/Main.java
+++ src/main/java/foo/Main.java
@@ -6,7 +6,15 @@ import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
 import co.paralleluniverse.strands.channels.ReceivePort;
 import co.paralleluniverse.strands.channels.SendPort;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -28,41 +36,46 @@ public class Main {
             System.out.println("ENV: ");
             System.out.println(printMap(System.getenv()));
             System.out.println("========================================");
+//            System.out.println("FILESYSTEM: ");
+//            dumpFileSystem(FileSystems.getDefault());
+//            System.out.println("========================================");
         }
 
-        final Channel<Integer> cin = Channels.newChannel(0);
+        if (true) {
+            final Channel<Integer> cin = Channels.newChannel(0);
 
-        new Fiber<Void>(() -> {
-            for (int i = 0; i < 10; i++) {
-                Strand.sleep(100);
-                cin.send(i);
-            }
-            cin.close();
-        }).start();
-
-        final Channel<Integer> cout = Channels.newChannel(0);
-
-        Channels.fiberTransform(cin, cout, (ReceivePort<Integer> in, SendPort<Integer> out) -> {
-            Integer x;
-            while ((x = in.receive()) != null) {
-                if (x % 2 != 0) {
-                    out.send(x);
-                    out.send(x * 10);
-                    out.send(x * 100);
+            new Fiber<Void>(() -> {
+                for (int i = 0; i < 10; i++) {
+                    Strand.sleep(100);
+                    cin.send(i);
                 }
-            }
-        });
+                cin.close();
+            }).start();
+
+            final Channel<Integer> cout = Channels.newChannel(0);
+
+            Channels.fiberTransform(cin, cout, (ReceivePort<Integer> in, SendPort<Integer> out) -> {
+                Integer x;
+                while ((x = in.receive()) != null) {
+                    if (x % 2 != 0) {
+                        out.send(x);
+                        out.send(x * 10);
+                        out.send(x * 100);
+                    }
+                }
+            });
 
 //        ReceivePort<Integer> cout = Channels.transform(cin)
 //                .filter(x -> x % 2 != 0)
 //                .flatmap(x -> Channels.toReceivePort(Arrays.asList(x, x * 10, x*100)));
-        Fiber<Void> f = new Fiber<Void>(() -> {
-            Integer i;
-            while ((i = cout.receive()) != null)
-                System.out.println("--> " + i);
-        }).start();
+            Fiber<Void> f = new Fiber<Void>(() -> {
+                Integer i;
+                while ((i = cout.receive()) != null)
+                    System.out.println("--> " + i);
+            }).start();
 
-        f.join();
+            f.join();
+        }
     }
 
     private static String printMap(Map<?, ?> map) {
@@ -71,5 +84,20 @@ public class Main {
             sb.append(entry.getKey()).append(": ").append(entry.getValue()).append('\n');
         });
         return sb.toString();
+    }
+
+    private static void dumpFileSystem(FileSystem fs) {
+        try {
+            Files.walkFileTree(fs.getRootDirectories().iterator().next(), new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(file);
+                    return super.visitFile(file, attrs);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
